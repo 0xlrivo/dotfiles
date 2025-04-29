2cefd7dd26086c76d1e85d2e117756a8a14669b6
lrivo <l.rivo04@gmail.com>
2025-04-29 10:37:57 +0200

    sonokai theme

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,11 @@
-return { 
-	"catppuccin/nvim", 
-	name = "catppuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme "catppuccin-macchiato"
-	end
+return {
+	'sainnhe/sonokai',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.sonokai_enable_italic = true
+        vim.cmd.colorscheme('sonokai')
+      		end
 }
